a68c6081a714fbf7f981d09e457a46b6b446f6be
Tansimrinky <tansimrinkybindu@gmail.com>
2023-10-21 19:09:14 +0600

    Brands added

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ require('dotenv').config()
 
 
 // middleware
-app.use(cors());
 app.use(express.json())
+app.use(cors());
+
 
 
 
@@ -29,28 +30,61 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productsCollections = client.db("ProductsDB").collection("Products");
+    const brandsCollection = client.db("brandsDB").collection("brands")
+
+    app.get('/products', async(req, res)=>{
+      const cursor = productsCollections.find();
+      const result = await cursor.toArray();
+      return res.send(result)
+    })
+
+    app.get('/brands', async(req, res) => {
+      const cursor = brandsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+  
+    app.post('/brands', async(req, res) => {
+      const brand = req.body;
+      console.log(brand);
+      const result = await brandsCollection.insertOne(brand)
+      res.send(result)
+    })
+
+
+    app.post('/products', async(req, res)=>{
+      const newProduct = req.body;
+      console.log(newProduct)
+      const result = await productsCollections.insertOne(newProduct)
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
 
 // jason static file
-app.use(express.static('public'));
-app.get('/brands', (req, res) => {
-    const brandData = require('public')
+// app.use(express.static('public'));
+// app.get('/brands', (req, res) => {
+//     const brandData = require('public')
 
-})
+// })
 
 
 app.get('/', (req, res ) => {
     res.send('ETechShop server is running.')
 })
+
+
 
 app.listen(port, () => {
     console.log(`ETechServer is running on port: ${port}`)
