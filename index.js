743a666d59519064e614bc363cc1b9f283b63aa9
Tansimrinky <tansimrinkybindu@gmail.com>
2023-10-24 18:48:33 +0600

    update, dlelete added

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 const express = require("express");
-const cors = require('cors');
-const app = express()
-const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
-
-
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
 // middleware
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
-
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fsmcn5d.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -23,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -32,45 +26,98 @@ async function run() {
     await client.connect();
 
     const productsCollections = client.db("ProductsDB").collection("Products");
-    const brandsCollection = client.db("brandsDB").collection("brands")
+    const brandsCollection = client.db("brandsDB").collection("brands");
+    const cartCollection = client.db("cartDB").collection("carts");
 
-    app.get('/products', async(req, res)=>{
+
+    app.get("/products", async (req, res) => {
       const cursor = productsCollections.find();
       const result = await cursor.toArray();
-      return res.send(result)
-    })
+      return res.send(result);
+    });
 
-    app.get('/brands', async(req, res) => {
+    app.get("/brands", async (req, res) => {
       const cursor = brandsCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/mycart", async (req, res) => {
+      const cursor = cartCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get('/products/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+      const result = await productsCollections.findOne(query)
       res.send(result)
     })
 
-  
-    app.post('/brands', async(req, res) => {
+    app.put('/products/:id', async(req, res) => {
+      const id = req.params.id;
+     const filter = { _id: new ObjectId(id)}
+     const options = { upsert: true}
+     const updatedProduct = req.body;
+     const product = {
+      $set : {
+        img: updatedProduct.img, 
+         name: updatedProduct.name,
+          brandName: updatedProduct.brandName,
+           types: updatedProduct.types,
+            price: updatedProduct.price,
+             description: updatedProduct.description,
+              rating: updatedProduct.rating
+      }
+     }
+     const result = await productsCollections.updateOne(filter, product, options)
+     res.send(result)
+    })
+
+    app.post("/brands", async (req, res) => {
       const brand = req.body;
       console.log(brand);
-      const result = await brandsCollection.insertOne(brand)
+      const result = await brandsCollection.insertOne(brand);
+      res.send(result);
+    });
+    app.post("/mycart", async (req, res) => {
+      const cartInfo = req.body;
+      console.log(cartInfo)
+      const uniqueCode = {
+        _id : new ObjectId()
+      }
+      // cartInfo.push(uniqueCode)
+      console.log(uniqueCode);
+      const result = await cartCollection.insertOne(cartInfo, uniqueCode);
       res.send(result)
-    })
+    });
 
-
-    app.post('/products', async(req, res)=>{
+    app.post("/products", async (req, res) => {
       const newProduct = req.body;
-      console.log(newProduct)
-      const result = await productsCollections.insertOne(newProduct)
+      console.log(newProduct);
+      const result = await productsCollections.insertOne(newProduct);
+      res.send(result);
+    });
+
+    app.delete('/mycart/:id', async(req, res) => {
+      const id = req.params.id;
+      console.log(id)
+      const query = {_id: new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query)
       res.send(result)
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
 
 // jason static file
 // app.use(express.static('public'));
@@ -79,13 +126,10 @@ run().catch(console.dir);
 
 // })
 
-
-app.get('/', (req, res ) => {
-    res.send('ETechShop server is running.')
-})
-
-
+app.get("/", (req, res) => {
+  res.send("ETechShop server is running.");
+});
 
 app.listen(port, () => {
-    console.log(`ETechServer is running on port: ${port}`)
-})
+  console.log(`ETechServer is running on port: ${port}`);
+});
